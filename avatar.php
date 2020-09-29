bebeb1992514b2702d907a6c0a87d29f7c26279c
vivifoa <vince.boellinger@outlook.fr>
2020-09-29 15:47:11 +0200

    fontionnement de m'avatar

--- avatar.php
+++ avatar.php
@@ -16,29 +16,27 @@ function avatar(int $size,array $colors){
     
         //on créé une boucle pour ajouter le nombre de couleur en fonction de la taille choisie
         for ($y=0 ; $y<$size ; $y++){
-
-        
-            //on tir au sort le choix des couleurs
-            $randcolors = array_rand(array_flip($colors), 1);
-        
-            //on l'ajoute au tableau
-            $tableauavatar[$x][$y]= $randcolors;
+     
+            if($y<$midsize) {
+                //on tir au sort le choix des couleurs
+                $randcolors = array_rand(array_flip($colors), 1);
+                
+                //on l'ajoute au tableau
+                $tableauavatar[$x][$y]= $randcolors;
+            } else {
+                $tableauavatar[$x][$y]=$tableauavatar[$x][$size-$y-1]; 
+            }
             
-            if ($x<$midsize) {
-                $pair = $tableauavatar[($x * $size) + $y] % 2 == 0;
-            } else { 
-                $pair = $tableauavatar[(($size - 1 - $x) * $size) + $y] % 2 == 0;}
         }
+    
     }
 
-    
-
-    return $pair;
+    return $tableauavatar;
 }
 
 
 
-$monavatar = avatar(6,["yellow","red","green"]);
+$monavatar = avatar(5,["yellow","red","green"]);
 
 //on affiche le resultat
 dump ($monavatar);
