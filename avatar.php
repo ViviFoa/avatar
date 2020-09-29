1563fd1f26f8c0f492ca07abab8922de24a8a6af
vivifoa <vince.boellinger@outlook.fr>
2020-09-29 15:41:59 +0200

    first commit

---
+++ avatar.php
@@ -0,0 +1,44 @@
+<?php
+
+require "vendor/autoload.php";
+
+
+function avatar(int $size,array $colors){
+    
+    $midsize = ceil($size/2);
+
+    //on créé un tableau 
+    $tableauavatar=[];
+
+    for ($x=0 ; $x<$size ; $x++) {
+
+        $tableauavatar[]=[];
+    
+        //on créé une boucle pour ajouter le nombre de couleur en fonction de la taille choisie
+        for ($y=0 ; $y<$size ; $y++){
+
+        
+            //on tir au sort le choix des couleurs
+            $randcolors = array_rand(array_flip($colors), 1);
+        
+            //on l'ajoute au tableau
+            $tableauavatar[$x][$y]= $randcolors;
+            
+            if ($x<$midsize) {
+                $pair = $tableauavatar[($x * $size) + $y] % 2 == 0;
+            } else { 
+                $pair = $tableauavatar[(($size - 1 - $x) * $size) + $y] % 2 == 0;}
+        }
+    }
+
+    
+
+    return $pair;
+}
+
+
+
+$monavatar = avatar(6,["yellow","red","green"]);
+
+//on affiche le resultat
+dump ($monavatar);
